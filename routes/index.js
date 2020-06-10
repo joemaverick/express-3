b282b139b6d2f7790c5d669daee07d982789b065
Joseph Yehulugeta <40561571+joemaverick@users.noreply.github.com>
2020-06-10 11:27:09 -0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  //res.send('respond with a resource');
   var title = "Our Home Page";
   res.render('pages/index',{
-    title:title,
+    title:title
   });
+});
+
 module.exports = router;
